21a52460f2c14cc227ef1c8cd1e40857b17e75dd
longtb <longtb@younetgroup.com>
2024-09-26 09:57:13 +0700

    restructure

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,69 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = void 0;
-__exportStar(require("./types"), exports);
-var main_1 = require("./main");
-Object.defineProperty(exports, "default", { enumerable: true, get: function () { return main_1.default; } });
+/**
+ * This class is designed for tracking and logging execution times for code sections.
+ * It allows for multiple log entries and provides options to display or return them for further analysis.
+ */
+class CodeExecutionTimer {
+    /**
+     * Initializes the timer with a label, starts tracking time, and sets up the log entries
+     * @param label
+     */
+    constructor(label) {
+        this.label = label;
+        this.initialTimestamp = Date.now();
+        this.previousTimestamp = this.initialTimestamp;
+        this.entries = [];
+    }
+    /**
+     * Logs the duration between the last recorded time and the current time, along with a description.
+     * @param description
+     * @param startTimestamp
+     * @returns
+     */
+    log(description, startTimestamp = this.previousTimestamp) {
+        const currentTimestamp = Date.now();
+        const executionTime = currentTimestamp - startTimestamp; // ms
+        this.entries.push({
+            description,
+            duration: executionTime,
+        });
+        this.previousTimestamp = currentTimestamp;
+        return this;
+    }
+    /**
+     * Returns the list of log entries, optionally printing them, and resets the timer for reuse.
+     * @param shouldPrint
+     * @returns
+     */
+    complete(shouldPrint = false) {
+        const loggedEntries = [...this.entries];
+        if (shouldPrint) {
+            this.printEntries(loggedEntries);
+        }
+        this.resetTimer();
+        return loggedEntries;
+    }
+    /**
+     * Resets the log entries and the timer
+     */
+    resetTimer() {
+        this.entries = [];
+        this.initialTimestamp = Date.now();
+        this.previousTimestamp = this.initialTimestamp;
+        return this;
+    }
+    /**
+     * Outputs the log entries to the console in a readable format.
+     * @param logEntries
+     */
+    printEntries(logEntries) {
+        const logOutput = [`[Execution Timer Log] ${this.label}`, '\n'];
+        logEntries.forEach(({ description, duration }) => {
+            logOutput.push(`* ${description} ===> ${duration}ms`);
+            logOutput.push('\n');
+        });
+        console.log(...logOutput);
+    }
+}
+exports.default = CodeExecutionTimer;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,92 @@
-export * from './types';
-export { default } from './main';
+export type LogEntry = {
+  /**
+   * A description of the code section being timed
+   */
+  description: string;
+  /**
+   * The elapsed time in milliseconds for this log entry
+   */
+  duration: number;
+}
+
+/**
+ * This class is designed for tracking and logging execution times for code sections.
+ * It allows for multiple log entries and provides options to display or return them for further analysis.
+ */
+export default class CodeExecutionTimer {
+  private label: string;
+  private entries: LogEntry[];
+  private initialTimestamp: number;
+  private previousTimestamp: number;
+
+  /**
+   * Initializes the timer with a label, starts tracking time, and sets up the log entries
+   * @param label
+   */
+  constructor(label: string) {
+    this.label = label;
+    this.initialTimestamp = Date.now();
+    this.previousTimestamp = this.initialTimestamp;
+    this.entries = [];
+  }
+
+  /**
+   * Logs the duration between the last recorded time and the current time, along with a description.
+   * @param description
+   * @param startTimestamp
+   * @returns
+   */
+  log(description: string, startTimestamp: number = this.previousTimestamp) {
+    const currentTimestamp = Date.now();
+    const executionTime = currentTimestamp - startTimestamp; // ms
+
+    this.entries.push({
+      description,
+      duration: executionTime,
+    });
+
+    this.previousTimestamp = currentTimestamp;
+
+    return this;
+  }
+
+  /**
+   * Returns the list of log entries, optionally printing them, and resets the timer for reuse.
+   * @param shouldPrint
+   * @returns
+   */
+  complete(shouldPrint = false) {
+    const loggedEntries = [...this.entries];
+    if (shouldPrint) {
+      this.printEntries(loggedEntries);
+    }
+
+    this.resetTimer();
+    return loggedEntries;
+  }
+
+  /**
+   * Resets the log entries and the timer
+   */
+  resetTimer() {
+    this.entries = [];
+    this.initialTimestamp = Date.now();
+    this.previousTimestamp = this.initialTimestamp;
+
+    return this;
+  }
+
+  /**
+   * Outputs the log entries to the console in a readable format.
+   * @param logEntries
+   */
+  printEntries(logEntries: LogEntry[]) {
+    const logOutput = [`[Execution Timer Log] ${this.label}`, '\n'];
+    logEntries.forEach(({ description, duration }) => {
+      logOutput.push(`* ${description} ===> ${duration}ms`);
+      logOutput.push('\n');
+    });
+
+    console.log(...logOutput);
+  }
+}
